f8828961e0a19ec198ad72f0aac2eae3c3bb5bc9
himayrob <160776152+himayrob@users.noreply.github.com>
2024-03-13 15:44:46 -0500

    vamos a crear nuestros propios ejercicios

---
+++ ejercicio loco .py
@@ -0,0 +1,13 @@
+dol = int(input("ingrese dolares a convertir"))
+
+dict:([
+  ('pesos', 5600),
+  ('soles', 105),
+  ('reais', 280)
+  ])
+
+def moneda *
+moneda = input("¿a que moneda desea convertir?")
+if moneda == "pesos" val.get ("pesos") * dol
+elif moneda == "soles" val.get ("soles") * dol
+else moneda == "reais" val.get ("reais") * dol
